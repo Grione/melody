126a454b844f224413446b08ad29e85d43b2d96b
Grione <greeb.spb@gmail.com>
2021-08-17 17:12:21 +0300

    add Enzyme lib and e2e test

---
+++ src/components/welcome-screen/welcome-screen.e2e.test.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Enzyme, { shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import WelcomeScreen from './welcome-screen';
+import { exportAllDeclaration } from '@babel/types';
+
+Enzyme.configure({ adapter: new Adapter() });
+
+it(`Should welcome button be pressed`, () => {
+	const onWelcomeButtonClick = jest.fn();
+
+	const welcomeScreen = shallow(
+		<WelcomeScreen errorCount={0} time={0} onWelcomeButtonClick={onWelcomeButtonClick} />
+	);
+
+	const startButton = welcomeScreen.find(`button`);
+	startButton.simulate(`click`);
+
+	expect(onWelcomeButtonClick).toHaveBeenCalledTimes(1);
+});
