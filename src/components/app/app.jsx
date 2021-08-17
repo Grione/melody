6f296283d50bcdc421dd0d073d9d678a640b9acb
Grione <greeb.spb@gmail.com>
2021-08-17 11:40:55 +0300

    add the App component

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -3,7 +3,7 @@ import WelcomeScreen from "../welcome-screen/welcome-screen";
 
 const App = (props) => {
 	const { gameTime, errorCount } = props;
-
+    console.log(props);
 	return <WelcomeScreen time={gameTime} errorCount={errorCount} />;
 };
 
