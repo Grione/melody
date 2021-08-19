fdaf81954cc05cec9ba4e6ee3e6d9c6acf6be998
Grione <greeb.spb@gmail.com>
2021-08-19 11:01:52 +0300

    add artist mock

--- src/mocks/questions.js
+++ src/mocks/questions.js
@@ -1,3 +1,5 @@
+const AVATAR_URL = `https://api.adorable.io/avatars/128`;
+
 export default [
 	{
 		type: `genre`,
@@ -18,6 +20,27 @@ export default [
 			{
 				src: `https://upload.wikimedia.org/wikipedia/commons/4/4e/BWV_543-fugue.ogg`,
 				genre: `rock`
+			}
+		]
+	},
+	{
+		type: `artist`,
+		song: {
+			artist: `Jim Beam`,
+			src: `https://upload.wikimedia.org/wikipedia/commons/4/4e/BWV_543-fugue.ogg`
+		},
+		answers: [
+			{
+				picture: `${AVATAR_URL}/${Math.random()}`,
+				artist: `John Daniels`
+			},
+			{
+				picture: `${AVATAR_URL}/${Math.random()}`,
+				artist: `John Daniels`
+			},
+			{
+				picture: `${AVATAR_URL}/${Math.random()}`,
+				artist: `John Daniels`
 			}
 		]
 	}
