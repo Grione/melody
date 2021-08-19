25043786b296328a8d8de90dc7c3535eece07069
Grione <greeb.spb@gmail.com>
2021-08-19 10:55:22 +0300

    add genre mock

--- src/components/welcome-screen/welcome-screen.e2e.test.js
+++ src/components/welcome-screen/welcome-screen.e2e.test.js
@@ -2,7 +2,6 @@ import React from 'react';
 import Enzyme, { shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import WelcomeScreen from './welcome-screen';
-import { exportAllDeclaration } from '@babel/types';
 
 Enzyme.configure({ adapter: new Adapter() });
 
