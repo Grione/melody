2a42162645c1d567635c6bc6e1665ba9a319e87b
Grione <greeb.spb@gmail.com>
2021-08-20 12:38:03 +0300

    create change screens logic

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,16 +1,56 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import WelcomeScreen from '../welcome-screen/welcome-screen';
+import GenreQuestionScreen from '../genre-question-screen/genre-question-screen';
+import ArtistQuestionScreen from '../artist-question-screen/artist-question-screen';
+import questions from '../../mocks/questions';
 
-const App = (props) => {
-	const { gameTime, errorCount, welcomeButtonHandler } = props;
-	return <WelcomeScreen time={gameTime} errorCount={errorCount} onWelcomeButtonClick={welcomeButtonHandler} />;
-};
+class App extends React.PureComponent {
+	static getScreen(question, props, onUserAnswer) {
+		if (question === -1) {
+			const { gameTime, errorCount } = props;
+			return <WelcomeScreen time={gameTime} errorCount={errorCount} onWelcomeButtonClick={onUserAnswer} />;
+		}
+		const { questions } = props;
+		const currentQuestion = questions[question];
+
+		switch (currentQuestion.type) {
+			case `genre`:
+				return <GenreQuestionScreen question={currentQuestion} onAnswer={onUserAnswer} />;
+			case `artist`:
+				return <ArtistQuestionScreen question={currentQuestion} onAnswer={onUserAnswer} />;
+		}
+
+		return null;
+	}
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			question: -1
+		};
+	}
+
+	render() {
+		const { question } = this.state;
+
+		return App.getScreen(question, this.props, () => {
+			this.setState((prevState) => {
+				const nextIndex = prevState.question + 1;
+				const isEnd = nextIndex >= questions.length;
+
+				return {
+					...prevState,
+					question: !isEnd ? nextIndex: -1,
+				};
+			});
+		});
+	}
+}
 
 App.propTypes = {
 	errorCount: PropTypes.number.isRequired,
-	gameTime: PropTypes.number.isRequired,
-	welcomeButtonHandler: PropTypes.func.isRequired
+	gameTime: PropTypes.number.isRequired
 };
 
 export default App;
