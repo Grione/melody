4e9de5aa28da0221634f916005bdca459a9af6a4
Grione <greeb.spb@gmail.com>
2021-09-07 14:00:32 +0300

    add new redux fuctionality to components

--- src/components/artist-question-screen/artist-question-screen.jsx
+++ src/components/artist-question-screen/artist-question-screen.jsx
@@ -12,77 +12,43 @@ class ArtistQuestionScreen extends React.PureComponent {
 	}
 
 	render() {
-		const { onAnswer, question } = this.props;
+		const { step, question, onAnswer } = this.props;
 		const { answers, song } = question;
 		const { isPlaying } = this.state;
 
 		return (
-			<section className="game game--artist">
-				<header className="game__header">
-					<a className="game__back" href="#">
-						<span className="visually-hidden">Сыграть ещё раз</span>
-						<img className="game__logo" src="img/melody-logo-ginger.png" alt="Угадай мелодию" />
-					</a>
-
-					<svg xmlns="http://www.w3.org/2000/svg" className="timer" viewBox="0 0 780 780">
-						<circle
-							className="timer__line"
-							cx="390"
-							cy="390"
-							r="370"
-							style={{
-								filter: `url(#blur)`,
-								transform: `rotate(-90deg) scaleY(-1)`,
-								transformOrigin: `center`
+			<section className="game__screen">
+				<h2 className="game__title">Кто исполняет эту песню?</h2>
+				<div className="game__track">
+					<div className="track">
+						<AudioPlayer
+							src={song.src}
+							isPlaying={isPlaying}
+							onPlayButtonClick={() => {
+								this.setState({ isPlaying: !isPlaying });
 							}}
 						/>
-					</svg>
-
-					<div className="timer__value" xmlns="http://www.w3.org/1999/xhtml">
-						<span className="timer__mins">05</span>
-						<span className="timer__dots">:</span>
-						<span className="timer__secs">00</span>
 					</div>
+				</div>
 
-					<div className="game__mistakes">
-						<div className="wrong" />
-						<div className="wrong" />
-						<div className="wrong" />
-					</div>
-				</header>
-
-				<section className="game__screen">
-					<h2 className="game__title">Кто исполняет эту песню?</h2>
-					<div className="game__track">
-						<div className="track">
-							<AudioPlayer
-								src={song.src}
-								isPlaying={isPlaying}
-								onPlayButtonClick={()=> {
-                  this.setState({ isPlaying: !isPlaying })
-                }}
+				<form className="game__artist">
+					{answers.map((answer, i) => (
+						<div key={answer.id} className="artist">
+							<input
+								className="artist__input visually-hidden"
+								type="radio"
+								name="answer"
+								value={`answer-${i}`}
+								id={`answer-${i}`}
+								onClick={() => onAnswer(answer)}
 							/>
+							<label className="artist__name" htmlFor={`answer-${i}`}>
+								<img className="artist__picture" src={answer.picture} alt={answer.artist} />
+								{answer.artist}
+							</label>
 						</div>
-					</div>
-
-					<form className="game__artist" onChange={onAnswer}>
-						{answers.map((answer, i) => (
-							<div key={answer.id} className="artist">
-								<input
-									className="artist__input visually-hidden"
-									type="radio"
-									name="answer"
-									value={`answer-${i}`}
-									id={`answer-${i}`}
-								/>
-								<label className="artist__name" htmlFor={`answer-${i}`}>
-									<img className="artist__picture" src={answer.picture} alt={answer.artist} />
-									{answer.artist}
-								</label>
-							</div>
-						))}
-					</form>
-				</section>
+					))}
+				</form>
 			</section>
 		);
 	}
